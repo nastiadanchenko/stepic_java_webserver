5cdfaa77b39fb92c5b169c0b9c6e789b1dca66fd
Anastasia Danchenko <nastiadanchenko@gmail.com>
2022-04-04 12:52:20 +0500

    Add new context ("/mirror")

--- L1.1 Simple web server/src/main/java/main/Main.java
+++ L1.1 Simple web server/src/main/java/main/Main.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
+import servlets.MirrorServlet;
 
 /**
  * @author v.chibrikov
@@ -16,13 +17,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
+        MirrorServlet mirrorServlet = new MirrorServlet();
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        context.addServlet(new ServletHolder(mirrorServlet), "/mirror");
 
         Server server = new Server(8080);
         server.setHandler(context);
 
         server.start();
+        context.getLogger().info("Server started");
         server.join();
     }
 }
