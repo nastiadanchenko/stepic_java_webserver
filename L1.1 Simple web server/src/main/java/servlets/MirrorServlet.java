615dd2d77e3b6579c51a93681ab699947fdfe9a2
Anastasia Danchenko <nastiadanchenko@gmail.com>
2022-04-04 15:38:38 +0500

    Add "Created by nastiadanchenko"

--- L1.1 Simple web server/src/main/java/servlets/MirrorServlet.java
+++ L1.1 Simple web server/src/main/java/servlets/MirrorServlet.java
@@ -7,15 +7,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * Created by nastiadanchenko
+ */
 public class MirrorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        Map<String, String[]> parameterMap = req.getParameterMap();
-//        if (parameterMap.containsKey("key")) {
-//            resp.getWriter().println(parameterMap.get("key")[0]);
-//
-//        }
-        resp.getWriter().println(req.getParameter("key"));
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        if (parameterMap.containsKey("key")) {
+            resp.getWriter().println(parameterMap.get("key")[0]);
+
+        }
+        //       resp.getWriter().println(req.getParameter("key"));
         resp.setContentType("text/html;charset=utf-8");
         resp.setStatus(HttpServletResponse.SC_OK);
     }
