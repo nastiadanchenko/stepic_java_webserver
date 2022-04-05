0a8e404e69f2284dd0ef231b17f869986c3b5c85
Anastasia Danchenko <nastiadanchenko@gmail.com>
2022-04-05 19:53:41 +0500

    Update Main.java and pom.xml

--- L5.1 Tests/src/main/java/example/Main.java
+++ L5.1 Tests/src/main/java/example/Main.java
@@ -12,6 +12,7 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AdminPageServlet;
 import servlets.HomePageServlet;
 
 import javax.management.MBeanServer;
@@ -19,12 +20,7 @@ import javax.management.ObjectName;
 import java.lang.management.ManagementFactory;
 
 /**
- * @author a.akbashev
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ * Created by nastiadanchenko
  */
 public class Main {
     static final Logger logger = LogManager.getLogger(Main.class.getName());
@@ -35,21 +31,27 @@ public class Main {
             System.exit(1);
         }
 
-        String portString = args[0];
+        String portString = args[0];//"5050";
         int port = Integer.valueOf(portString);
 
         logger.info("Starting at http://127.0.0.1:" + portString);
 
-        AccountServerI accountServer = new AccountServer(1);
+        AccountServerI accountServer = new AccountServer(10);
 
         AccountServerControllerMBean serverStatistics = new AccountServerController(accountServer);
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
         ObjectName name = new ObjectName("ServerManager:type=AccountServerController");
+
+        ObjectName name1 = new ObjectName("Admin:type=AccountServerController");
+
         mbs.registerMBean(serverStatistics, name);
+        mbs.registerMBean(serverStatistics, name1);
 
         Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
         context.addServlet(new ServletHolder(new HomePageServlet(accountServer)), HomePageServlet.PAGE_URL);
+        context.addServlet(new ServletHolder(new AdminPageServlet(accountServer)), AdminPageServlet.PAGE_URL);
 
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setDirectoriesListed(true);
@@ -61,7 +63,6 @@ public class Main {
 
         server.start();
         logger.info("Server started");
-
         server.join();
     }
 }
